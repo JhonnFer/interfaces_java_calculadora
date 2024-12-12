8927805363055412173fd14d4983edf36f7f7041
JhonnFer <jhoncasq23@gmail.com>
2024-12-12 08:57:20 -0500

    ACTUALIZACION

--- src/form3.java
+++ src/form3.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 
 public class form3 {
     private JTextArea TRANSACCIONREALIZARTextArea;
-    private JRadioButton radioButton1;
-    private JRadioButton radioButton2;
-    private JRadioButton radioButton3;
-    private JRadioButton radioButton4;
+    private JRadioButton VERSALDORadioButton;
+    private JRadioButton RETIRORadioButton;
+    private JRadioButton DEPOSITORadioButton;
+    private JRadioButton SALIRRadioButton;
 }
